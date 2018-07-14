5c9431db1820e36e4f037ab00f7522dfba4ae245
mark <huihappygeek@gmail.com>
2018-07-15 00:30:13 +0800

    0714

--- woaipython/0714.py
+++ woaipython/0714.py
@@ -5,6 +5,22 @@ print(a);
 '''
 
 # name=input();
-
-name=input();
+'''
+name=input('请输入你的名字：');
 print('hello',name);
+'''
+
+# print('1024 x 768 =',1024*768);
+# print absolute value of an interger
+'''
+a=int(input('请输入一个数：'));
+if a >=0:
+    print('绝对数为：',a)
+else:
+    print(-a)
+'''
+
+
+
+
+
